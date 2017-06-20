3ed52461d89b1bf88177eba39556295e6062c4ae
Cheshire7 <test@test.test>
2017-06-20 15:46:57 +0300

    newCommit

--- public/app.ts
+++ public/app.ts
@@ -1,15 +1,14 @@
 import './styles/styles.scss';
-//import angular from 'angular';
+import angular from 'angular';
 
+import { Component } from '@angular/core';
 
-// import { Component } from '@angular/core';
-     
-// @Component({
-//     selector: 'my-app',
-//     template: `<label>Введите имя:</label>
-//                  <input [(ngModel)]="name" placeholder="name">
-//                  <h1>Добро пожаловать {{name}}!</h1>`
-// })
-// export class AppComponent { 
-//     name: "";
-// }
+@Component({
+    selector: 'my-app',
+    template: `<label>Введите имя:</label>
+                 <input [(ngModel)]="name" placeholder="name">
+                 <h1>Добро пожаловать {{name}}!</h1>`
+})
+export class AppComponent { 
+    name: "";
+}
