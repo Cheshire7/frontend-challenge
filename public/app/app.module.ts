642ea1a141f3632534643418348da2e2a5c7f69a
Cheshire7 <test@test.test>
2017-06-21 13:24:20 +0300

    Temporary commit

--- public/app/app.module.ts
+++ public/app/app.module.ts
@@ -2,10 +2,9 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
-import { HttpModule }   from '@angular/http';
  
 @NgModule({
-    imports:[BrowserModule, FormsModule, HttpModule],
+    imports:[BrowserModule, FormsModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
