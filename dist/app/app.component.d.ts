85e487885c0a8f41e30938f85e08ce14cef05b7d
Cheshire7 <test@test.test>
2017-06-21 12:07:03 +0300

    base_structure

--- dist/app/app.component.d.ts
+++ dist/app/app.component.d.ts
@@ -1,2 +1,6 @@
 export declare class AppComponent {
+    names: string[];
+    age: number;
+    constructor();
+    UserAdd(newtitle: HTMLInputElement, newposition: HTMLInputElement): boolean;
 }
--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -1,7 +1,44 @@
-import { Component} from '@angular/core';
+import {Component} from '@angular/core';
  
 @Component({
     selector: 'my-app',
-    template: `<h2>Hello Angular! Welcome Webpack!</h2>`
+    template: `
+    			<ul *ngIf='names.length'>
+    				<li *ngFor='let name of names'>{{name}}</li>
+    			</ul>
+    			
+    			<form>
+				<h3>Добавить сотрудника</h3>
+
+				<div class='field'>
+					<label for='title'>Имя</label>
+					<input type='text' name='name' #newtitle>
+				</div>
+
+				<div class='field'>
+					<label for='position'>Должность</label>
+					<input type='text' name='position' #newposition>
+				</div>
+                <button (click)='UserAdd(newtitle, newposition)'>Отправить</button>
+            </form>
+    		`
 })
-export class AppComponent { }
+
+export class AppComponent { 
+
+	//simple DB
+	names: string[];
+    age: number;
+
+    constructor() {
+    	this.names = ['user1', 'user2', 'user3'];
+
+    }
+    //button event
+    UserAdd(newtitle: HTMLInputElement, newposition: HTMLInputElement) {
+    	console.log('Имя' + newtitle.value);
+    	console.log('Должность' + newposition.value); 
+    	return false;
+    }
+
+}
