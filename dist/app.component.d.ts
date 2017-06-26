3089424fcbea87c1b537b16b5e86d546a9a5e7dd
Cheshire7 <test@test.test>
2017-06-26 12:22:43 +0300

    mockup

--- dist/app.component.d.ts
+++ dist/app.component.d.ts
@@ -1,9 +1,22 @@
 import { OnInit } from '@angular/core';
 import { HttpService } from './http.service';
 import './styles/styles.scss';
+export declare class User {
+    id: number;
+    photo: string;
+    name: string;
+    lastName: string;
+    pol: string;
+    birthday: string;
+    position: string;
+    skill: string;
+    characteristic: string;
+}
 export declare class AppComponent implements OnInit {
     private httpService;
+    selectedUser: User;
     user_list: Array<any>;
     constructor(httpService: HttpService);
     ngOnInit(): void;
+    onSelect(user: User): void;
 }
--- public/app.component.ts
+++ public/app.component.ts
@@ -1,8 +1,18 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { HttpService } from './http.service';
-
 import './styles/styles.scss';
 
+export class User {
+    id: number;
+    photo: string;
+    name: string;
+    lastName: string;
+    pol: string;
+    birthday: string;
+    position: string;
+    skill: string;
+    characteristic: string;
+}
 
 @Component({
     selector: 'my-app',
@@ -12,13 +22,19 @@ import './styles/styles.scss';
 
 export class AppComponent implements OnInit {
 
+    selectedUser: User;
+
     user_list: Array<any>;
 
     constructor(@Inject(HttpService) private httpService: HttpService) {
 
-    }
-
-    ngOnInit() {
-        this.httpService.getData().subscribe(users => this.user_list = users);
-    }
 }
+
+ngOnInit() {
+    this.httpService.getData().subscribe(users => this.user_list = users);
+}
+
+onSelect(user: User) {
+    this.selectedUser = user;
+}
+} 
